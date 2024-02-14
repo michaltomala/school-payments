b9dd20d31fba39999052871cfb95517cd5d11412
Michał Tomala <michaltomala63@gmail.com>
2024-02-15 00:56:45 +0100

    project initialization

---
+++ src/main/java/com/example/schoolpayments/SchoolPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolpayments/SchoolPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
